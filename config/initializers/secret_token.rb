beba32ee97664d8439715b407d54b9b797ca5d0c
Lee Gillentine <lgillentine@gmail.com>
2014-04-14 21:37:08 -0500

    Inital checkin of rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eyebleach::Application.config.secret_key_base = '0dd8a49128b69fac90dd6b0131af72d170fc5f467cd7b94160f74f13c069b2202b9204adb868308605ad66514406368f0036fe0fd960d87b6ff8eb1cda8d505e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eyebleach::Application.config.session_store :cookie_store, key: '_eyebleach_session'
